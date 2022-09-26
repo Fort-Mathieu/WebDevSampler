b322f84fec1cb758e67ad37274dcf79251a18651
Mathieu Fort <mathieu.fort86@gmail.com>
2022-09-26 14:18:22 +0200

    Add several Test

--- test/main.test.ts
+++ test/main.test.ts
@@ -1,19 +1,19 @@
 import { describe, expect, test } from "@jest/globals";
-import { startServer, killServer } from "../main";
+import { startServer } from "../main";
 import http from "http";
 
-const port = 3001;
+const port = 3005;
 
 beforeAll(() => {
   startServer(port);
 });
 
 afterAll(() => {
-  killServer();
+  //killServer();
 });
 
-describe("Hello World", () => {
-  test('should call "/" and return "Hello World"', () => {
+describe("Main Test", () => {
+  test('should call "/" and return "Hello World!"', () => {
     let body = "";
 
     http.get(`http://localhost:${port}`, (res) => {
@@ -22,6 +22,37 @@ describe("Hello World", () => {
       });
       res.on("end", () => {
         expect(body.toString().length > 0);
+        expect(body.toString()).toEqual("Hello World!");
+      });
+    });
+  });
+
+  test('should call "/about" and return the correct string', () => {
+    let body = "";
+
+    http.get(`http://localhost:${port}/about`, (res) => {
+      res.on("data", (chunk) => {
+        body += chunk;
+      });
+      res.on("end", () => {
+        expect(body.toString().length > 0);
+        expect(body.toString()).toEqual("this is my about page");
+      });
+    });
+  });
+
+  test('should call "/signup/my-name-is/Tom" and return "You\'re all signed up for the big convention Tom"', () => {
+    let body = "";
+
+    http.get(`http://localhost:${port}/signup/my-name-is/Tom`, (res) => {
+      res.on("data", (chunk) => {
+        body += chunk;
+      });
+      res.on("end", () => {
+        expect(body.toString().length > 0);
+        expect(body.toString()).toEqual(
+          "You're all signed up for the big convention Tom"
+        );
       });
     });
   });
